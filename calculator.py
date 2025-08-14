d1bca8c6094cf7b126e29b8e3cbe16c5172dc9e1
Márcio Mavungo <marcmav@proton.me>
2025-08-14 14:27:40 +0100

    Update calculator.py
    improved error handling and aesthetics

--- calculator.py
+++ calculator.py
@@ -12,38 +12,46 @@ def division(a, b):
 
 while True:
     try:
-        print("Choose operation (1-5):")
+        print("Would you like to? ")
         print("""1. Add.
 2. Subtract.
 3. Multiply.
 4. Divide.
 5. Exit.""")
-        operation = int(input("Would you like to? "))
+        operation = int(input("Choose an operation (1-5): "))
     except ValueError:
-        print("To choose an operation enter the number assigned to the operation. TRY AGAIN.\n")
+        print("\nTo choose an operation, enter the number assigned to the operation. \nTRY AGAIN.\n")
+    except Exception as e:
+        print(f"{e}\n")
     else:
-        if operation == 1:
-            a = int(input("Enter the first number: "))
-            b = int(input("Enter the second number: "))
-            add = sum(a, b)
-            print(f"{a} + {b} = {add}")
-        elif operation == 2:
-            a = int(input("Enter the first number: "))
-            b = int(input("Enter the second number: "))
-            subtract = subtraction(a, b)
-            print(f"{a} - {b} = {subtract}")    
-        elif operation == 3:
-            a = int(input("Enter the first number: "))
-            b = int(input("Enter the second number: "))
-            multiply = multiplication(a, b)
-            print(f"{a} * {b} = {multiply}")    
-        elif operation == 4:
-            a = int(input("Enter the first number: "))
-            b = int(input("Enter the second number: "))
-            divide = division(a, b)
-            print(f"{a} / {b} = {divide}")   
-        elif operation == 5:
-            break
+        try:
+            if operation == 1 or operation == 2 or operation == 3 or operation == 4:
+                a = int(input("Enter the first number: "))
+                b = int(input("Enter the second number: "))
+        except ValueError:
+                print("\nPlease enter a valid numeric value.\n")
+        except Exception as e:
+            print(f"{e}\n")
         else:
-            print("Chose a valid operation. TRY AGAIN. \n")
-            continue
+            if operation == 1:
+                add = sum(a, b)
+                print(f"{a} + {b} = {add}\n")
+            elif operation == 2:
+                subtract = subtraction(a, b)
+                print(f"{a} - {b} = {subtract}\n")    
+            elif operation == 3:
+                multiply = multiplication(a, b)
+                print(f"{a} * {b} = {multiply}\n")    
+            elif operation == 4:
+                try:
+                    divide = division(a, b)
+                    print(f"{a} / {b} = {divide}\n")   
+                except ZeroDivisionError: 
+                    print("\nYou can't divide by zero.\n")
+                except Exception as e:
+                    print(f"{e}\n")
+            elif operation == 5:
+                break
+            else:
+                print("\nChoose a valid operation. \nTRY AGAIN. \n")
+                continue
