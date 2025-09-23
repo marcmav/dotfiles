05224ba86adda3dd7ebb2b9845cc6d299737b447
Marciano Mavungo <marcmav.dev@gmail.com>
2025-09-23 16:12:30 +0100

    practice

--- js/tests.js
+++ js/tests.js
@@ -1,1 +1,10 @@
-console.log('hello motherfucker')
+// Write code below 💖
+
+const player = 0;
+let computer = Math.floor(Math.random() * 10);
+if (player === 0 && computer !== 1 || player === 1 && computer !== 2 || player === 2 && computer !== 0) {
+  console.log('player wins');
+}
+else {
+    console.log('player losses')
+}
