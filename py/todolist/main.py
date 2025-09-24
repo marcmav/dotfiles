4d702b248c27f4db0a4c1ff3f4e8d6906b243b4e
Marciano Mavungo <marcmav.dev@gmail.com>
2025-09-24 16:38:48 +0100

    update todo list

--- py/todolist/main.py
+++ py/todolist/main.py
@@ -1,15 +1,26 @@
 class Todo:
     def add_task(self, task):
         self.task = task
-        with open('todo.txt', 'a') as td:
-            td.write(task + '\n')
+        with open('todo', 'a') as f:
+            f.write(task + '\n')
 
     def read_tasks(self):
-        with open('todo.txt', 'r') as td:
-            print(td.read())
+        with open('todo', 'r') as f:
+            print(f.read())
 
-    
-
-
+    def remove_task(self, line):
+        self.line = line
+        with open('todo', 'r') as f:
+            td = f.read()
+            count = 0
+            new_f = []
+            for l in td:
+                count += 1
+                if count == self.line:
+                    continue
+                else:
+                    new_f.append(l)
+            
+        print(new_f)
 new = Todo()
-# new.read_tasks()
+new.remove_task(5)
