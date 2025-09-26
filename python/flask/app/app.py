37b8584b9ba9f745a53e78573437101a72f4a1d4
Marciano Mavungo <marcmav.dev@gmail.com>
2025-09-26 20:33:37 +0100

    update app.py

--- python/flask/app/app.py
+++ python/flask/app/app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello {name}'
+
+@app.route('/<int:a>/<int:b>')
+def multiply(a, b):
+    return f'{a} * {b} = {a*b}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
