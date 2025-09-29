f0fef10b9d8f565906a627bece02314d9be01fd0
Marciano Mavungo <marcmav@proton.me>
2025-09-29 17:35:04 +0100

    update app

--- python/app/app.py
+++ python/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder = 'templates')
 
@@ -7,6 +7,13 @@ app = Flask(__name__, template_folder = 'templates')
 def index():
     return render_template('index.html')
 
-    
+@app.route('/second')
+def second():
+    return 'this is the second page'
+
+@app.route('/home')
+def home():
+    return redirect(url_for('/'))
+
 if __name__ == '__main__':
     app.run(debug=True)
