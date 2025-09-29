3718840c549723091a899bd141368eafd324828f
Marciano Mavungo <marcmav@proton.me>
2025-09-29 16:45:06 +0100

    update app

--- python/flask/app/app.py
+++ python/flask/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -8,23 +8,5 @@ def index():
     return 'This is the home page'
 
     
-
-@app.route('/home')
-def greet():
-    response = make_response() 
-    response.status_code = 201
-    response.headers['content type'] = 'python/text'
-    return response
-
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f'{greeting}, {name}'
-    else:
-        return 'some parameters are missing'
-
-
 if __name__ == '__main__':
     app.run(debug=True)
