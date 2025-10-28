4263f2f7b8fbf9c077631fb01933a00ccfaa8eab
Marciano Mavungo <marcmav@proton.me>
2025-10-28 15:03:06 +0100

    update calculator

--- src/calculator/main.js
+++ src/calculator/main.js
@@ -3,7 +3,7 @@ const subtraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
 const division = (a, b) => a / b;
 do {
-    console.log("Would you like to? ");
+    console.log('Would you like to? ');
     console.log(`1. Add.
         2. Subtract.
         3. Multiply.
