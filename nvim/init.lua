0d4caf731dfde9c8f60d9d180f4b5136e4f7e002
Marciano Mavungo <marcmav.dev@gmail.com>
2026-03-16 11:09:10 +0100

    add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
+
+-- open neotree with space + e
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+
+-- ctrl + key arrows to change focus between windows (splits)
+vim.keymap.set("n", "<C-h>", "<C-w>h") -- ctrl + h = left window focus
+vim.keymap.set("n", "<C-j>", "<C-w>j") -- ctrl + j = down window focus
+vim.keymap.set("n", "<C-k>", "<C-w>k") -- ctrl + k = up window focus
+vim.keymap.set("n", "<C-l>", "<C-w>l") -- ctrl + l = left window focus
+
+-- refactor
+vim.keymap.set("n", "B", "^") -- "^" to B
+vim.keymap.set("n", "E", "$") -- "$" to E
+
+-- space  + tn (tabnew) and space + tc (tabclose)
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>") -- space + tn == tabnew
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>") -- space + tc == tabclose
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/marcio/.config/omarchy/current/theme/neovim.lua
