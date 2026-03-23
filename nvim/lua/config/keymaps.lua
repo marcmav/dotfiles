ccfc6f74387c20f2826c540c3222c40bbe79bdde
Marciano Mavungo <marcmav@proton.me>
2026-03-23 17:57:43 +0100

    clean init.lua and remap keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,37 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-------------------------REMAP-------------------------
+-------------------------------------------------------
+
+vim.keymap.set({ "n", "v" }, "B", "0") -- "0" to BB
+vim.keymap.set({ "n", "v" }, "BB", "^") -- "^" to B
+vim.keymap.set({ "n", "v" }, "E", "$") -- "$" to E
+
+-- toggle normal mode and terminal mode with "jj"
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
+vim.keymap.set("t", "jj", "<C-\\><C-n>", { noremap = true, silent = true })
+
+-- open neotree with space + e
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+
+-- ctrl + key arrows to change focus between windows (splits)
+vim.keymap.set("n", "<C-h>", "<C-w>h") -- ctrl + h = left window focus
+vim.keymap.set("n", "<C-j>", "<C-w>j") -- ctrl + j = down window focus
+vim.keymap.set("n", "<C-k>", "<C-w>k") -- ctrl + k = up window focus
+vim.keymap.set("n", "<C-l>", "<C-w>l") -- ctrl + l = left window focus
+
+-- open and close terminal with space + t
+vim.keymap.set("n", "<leader>t", function()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].buftype == "terminal" then
+      vim.api.nvim_buf_delete(buf, { force = true })
+      return
+    end
+  end
+  vim.cmd("split | terminal")
+end)
+
+-------------------------------------------------------
+-------------------------------------------------------
