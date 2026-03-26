e129c30b1edff542f5d4e17ef8a78f2e3662e310
Marciano Mavungo <marcmav.dev@gmail.com>
2026-03-26 22:39:31 +0100

    add autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  callback = function()
+    -- open Neo-tree when a real file buffer is loaded
+    if vim.bo.buftype == "" then
+      vim.cmd("Neotree show")
+    end
+  end,
+})
