e841ddd2170a1d475030e164a318e697ecaa7f5b
Marciano Mavungo <marcmav@proton.me>
2026-05-15 10:40:16 +0100

    disable noice

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end -- (override)
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -9,6 +9,15 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+
+  -- disable noice.nvim (override)
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+
+  --[[
+
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
@@ -171,7 +180,7 @@ return {
     event = "VeryLazy",
     opts = function()
       return {
-        --[[add your custom lualine config here]]
+        --add your custom lualine config here
       }
     end,
   },
@@ -194,4 +203,7 @@ return {
       },
     },
   },
+
+  ]]
+
 }
