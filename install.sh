ae09b08af1a2a31e7a953f773771bdb7138e6fe4
Marciano Mavungo <marcmav@proton.me>
2026-08-11 18:48:51 +0100

    update script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+DE=
 
+read -p "enter your prefered development environment (absolute path eg. '~/Code/open-source/'): " DE
+
+cd $(DE) || mkdir $(DE)
+git clone github.com/marcmav/dotfiles.git
+cd dotfiles/
